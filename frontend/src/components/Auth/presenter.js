78c2ef654854f55aa86724154c3833fc10c77936
ChicRock <jungsungrak@gmail.com>
2018-03-18 18:28:48 +0900

    Add context in stateless components

--- frontend/src/components/Auth/presenter.js
+++ frontend/src/components/Auth/presenter.js
@@ -1,44 +1,50 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './styles.scss';
 import { LoginForm, SignupForm } from 'components/AuthForms';
 
 const Auth = (props, context) => (
     <main className={styles.auth}>
         <div className={styles.column}>
-            <img src={require('images/phone.png')} alt="Checkout our app. Is cool" />
+            <img src={require('images/phone.png')} alt={context.t('Checkout our app. Is cool')} />
         </div>
         <div className={styles.column}>
             <div className={`${styles.whiteBox} ${styles.formBox}`}>
-                <img src={require('images/logo.png')} alt="Logo" />
+                <img src={require('images/logo.png')} alt={context.t('Logo')} />
                 {props.action === 'login' && <LoginForm />}
                 {props.action === 'signup' && <SignupForm />}
             </div>
             <div className={styles.whiteBox}>
                 {props.action === 'login' && (
                     <p className={styles.text}>
-                        Don't have an account?{' '}
+                        {context.t("Don't have an account?")}{' '}
                         <span className={styles.changeLink} onClick={props.changeAction}>
-                            Sign up
+                            {context.t('Sign up')}
                         </span>
                     </p>
                 )}
                 {props.action === 'signup' && (
                     <p className={styles.text}>
-                        Have an account?{' '}
+                        {context.t('Have an account?')}{' '}
                         <span className={styles.changeLink} onClick={props.changeAction}>
-                            Log in
+                            {context.t('Log in')}
                         </span>
                     </p>
                 )}
             </div>
             <div className={styles.appBox}>
-                <span>Get the App</span>
+                <span>{context.t('Get the App')}</span>
                 <div className={styles.appstores}>
-                    <img src={require('images/ios.png')} alt="Download it on the Apple Appstore" />
-                    <img src={require('images/android.png')} alt="Download it on the Google Playstore" />
+                    <img src={require('images/ios.png')} alt={context.t('Download it on the Apple Appstore')} />
+                    <img src={require('images/android.png')} alt={context.t('Download it on the Google Playstore')} />
                 </div>
             </div>
         </div>
     </main>
 );
+
+Auth.contextTypes = {
+    t: PropTypes.func.isRequired,
+};
+
 export default Auth;
--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -2,5 +2,15 @@ export const translations = {
     ko: {
         'Log in': '로그인 하였습니다',
         'About Us': 'Nomadgram 정보',
+        Support: '지원',
+        Blog: '블로그',
+        Press: '홍보 센터',
+        API: 'API',
+        Jobs: '채용 정보',
+        Privacy: '개인정보처리방침',
+        Terms: '약관',
+        Directory: '디렉토리',
+        Hashtag: '해쉬태그',
+        Language: '언어',
     },
 };
