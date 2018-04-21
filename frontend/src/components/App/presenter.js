da2d3823e55c28828f188fead248569f36116518
ChicRock <jungsungrak@gmail.com>
2018-04-21 13:05:41 +0900

    Change route order for search

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -10,30 +10,30 @@ import Explore from "components/Explore";
 import Search from "components/Search";
 import Profile from "components/Profile";
 
-const App = (props) => [
-    props.isLoggedIn ? <Navigation key={1} /> : null,
-    props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
-    <Footer key={3} />,
+const App = props => [
+  props.isLoggedIn ? <Navigation key={1} /> : null,
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />,
 ];
 
 App.propTypes = {
-    isLoggedIn: PropTypes.bool.isRequired,
+  isLoggedIn: PropTypes.bool.isRequired,
 };
 
-const PrivateRoutes = (props) => (
-    <Switch>
-        <Route exact path="/" component={Feed} />
-        <Route path="/explore" component={Explore} />
-        <Route path="/:userName" component={Profile} />
-        <Route path="/search/:searchTerm" component={Search} />
-    </Switch>
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Feed} />
+    <Route path="/explore" component={Explore} />
+    <Route path="/search/:searchTerm" component={Search} />
+    <Route path="/:userName" component={Profile} />
+  </Switch>
 );
 
-const PublicRoutes = (props) => (
-    <Switch>
-        <Route exact path="/" component={Auth} />
-        <Route path="/forgot" render={() => "password"} />
-    </Switch>
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Auth} />
+    <Route path="/forgot" render={() => "password"} />
+  </Switch>
 );
 
 export default withRouter(App);
